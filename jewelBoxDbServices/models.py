c11d4aa722223828f19e01a6d41bc1a026dc2297
EC2 Default User <ec2-user@ip-172-31-5-19.eu-west-1.compute.internal>
2025-04-04 20:27:38 +0000

    test

--- jewelBoxDbServices/models.py
+++ jewelBoxDbServices/models.py
@@ -30,7 +30,7 @@ class Jewelry(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default="Uncategorized")
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
