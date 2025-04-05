efd90be5ac23780bbe29849c7bb88c1e699c5ea0
EC2 Default User <ec2-user@ip-172-31-5-19.eu-west-1.compute.internal>
2025-04-05 11:34:03 +0000

    test

--- jewelBox/settings.py
+++ jewelBox/settings.py
@@ -72,14 +72,6 @@ WSGI_APPLICATION = 'jewelBox.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
