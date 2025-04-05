555d767252c41232c540268394893da77e75bec2
EC2 Default User <ec2-user@ip-172-31-5-19.eu-west-1.compute.internal>
2025-04-05 16:07:45 +0000

    static

--- jewelBox/settings.py
+++ jewelBox/settings.py
@@ -130,6 +130,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'frontEnd/media/')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontEnd')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontEnd/static')]
 
 # Default primary key field type
