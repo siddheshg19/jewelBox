93b729dbf3d89fd2768fb1042d3db44776f8d525
EC2 Default User <ec2-user@ip-172-31-5-19.eu-west-1.compute.internal>
2025-04-05 12:33:14 +0000

    build

--- jewelBox/settings.py
+++ jewelBox/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-+9mzsv^njq%-g_gp$ff%rvap9-yfzyswy0^asl$(e@^way=8vr'
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
